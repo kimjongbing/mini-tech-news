111454486f61d136ee6ab129ede6bfb75ccaf746
tooe <bobtail-ivy-sliver@duck.com>
2024-12-31 13:12:18 +1100

    ADD: Working api calls

--- src/main.py
+++ src/main.py
@@ -1,23 +1,24 @@
 import requests
 
 headers = {
-    'User-Agent': 'Mozilla/5.0',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Origin': 'https://techurls.com',
-    'Referer': 'https://techurls.com/'
+    "User-Agent": "Mozilla/5.0",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Origin": "https://techurls.com",
+    "Referer": "https://techurls.com/",
 }
 
-data = {
-    'site': 'hackernews',
-    'interval': 'latest',
-    'last_id': '1089324',
-    'load_more': 'true',
-    'u': '2',
-    'v': '194334'
-}
+data = {"site": "hackernews", "interval": "latest", "u": "2", "v": "194334"}
 
-response = requests.post('https://techurls.com/api/get_titles', 
-                        headers=headers,
-                        data=data)
+response = requests.post(
+    "https://techurls.com/api/get_titles", headers=headers, data=data
+)
 
-print(response.json())
+if response.status_code == 200:
+    stories = response.json()
+    print(stories)
+    if stories["status"] == "success":
+        newest_id = stories["data"][0]["id"]
+        print(f"Newest ID: {newest_id}")
+else:
+    print(f"Error: {response.status_code}")
+    print(response.text)
